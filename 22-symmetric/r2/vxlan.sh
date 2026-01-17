d4eda2a8fcd2ae2c3edfac65d90c14b1a6e94bcc
JohanOnPc <johan.beime@gmail.com>
2026-01-17 14:21:18 +0100

    Started working on a lab to look at how EVPN type-5 prefix routes are installed into the linux kernel

---
+++ 22-symmetric/r2/vxlan.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ip link add vrf-blue type vrf table 1000
+ip link set vrf-blue up
+
+ip link add br1000 type bridge
+ip link set br1000 master vrf-blue addrgenmode none
+ip link add vxlan1000 type vxlan id 1000 local 10.0.0.2 dstport 4789 nolearning
+ip link set vxlan1000 addr aa:bb:cc:dd:02:01
+ip link set vxlan1000 master br1000 addrgenmode none
+ip link set vxlan1000 type bridge_slave neigh_suppress on learning off
+ip link set br1000 up
+ip link set vxlan1000 up
+ip link set eth2 master vrf-blue
+
+ip route add 172.16.3.0/24 via 10.0.0.3 dev br1000 onlink vrf vrf-blue
+ip route add 172.16.4.0/24 via 10.0.0.4 dev br1000 onlink vrf vrf-blue
+
+ip neigh add 10.0.0.3 lladdr aa:bb:cc:dd:03:01 dev br1000 nud noarp extern_learn
+ip neigh add 10.0.0.4 lladdr aa:bb:cc:dd:04:01 dev br1000 nud noarp extern_learn
+
+bridge fdb add aa:bb:cc:dd:03:01 dev vxlan1000 master extern_learn
+bridge fdb add aa:bb:cc:dd:03:01 dev vxlan1000 dst 10.0.0.3 self extern_learn
+bridge fdb add aa:bb:cc:dd:04:01 dev vxlan1000 master extern_learn
+bridge fdb add aa:bb:cc:dd:04:01 dev vxlan1000 dst 10.0.0.4 self extern_learn
