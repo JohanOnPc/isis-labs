c4e415e512d7255279459b293079868a8b39dd53
JohanOnPc <johan.beime@gmail.com>
2026-01-18 02:15:49 +0100

    Spanned a vlan over two routers: R3 and R4, for which R2 has the equivalent of an EVPN type 5 route towards the two routers, thus ecmp is used. Because of this, not always the correct VTEP will be picked, and thus the L2VXLAN will need to bridge the packet  to the correct destination

--- 22-symmetric/r2/vxlan.sh
+++ 22-symmetric/r2/vxlan.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#vrf blue
 ip link add vrf-blue type vrf table 1000
 ip link set vrf-blue up
 
@@ -17,3 +18,23 @@ ip neigh add 10.0.0.4 lladdr aa:bb:cc:dd:ee:04 dev vxlan1000 nud noarp extern_le
 
 bridge fdb add aa:bb:cc:dd:ee:03 dev vxlan1000 dst 10.0.0.3 self extern_learn
 bridge fdb add aa:bb:cc:dd:ee:04 dev vxlan1000 dst 10.0.0.4 self extern_learn
+
+#vrf red
+ip link add vrf-red type vrf table 1100
+ip link set vrf-red up
+
+ip link add vxlan1100 type vxlan id 1100 local 10.0.0.2 dstport 4789 nolearning
+ip link set vxlan1100 addr aa:bb:cc:dd:ee:02
+ip link set vxlan1100 master vrf-red addrgenmode none
+ip link set vxlan1100 up
+ip link set eth3 master vrf-red
+
+ip route add 192.168.3.0/24 vrf vrf-red \
+    nexthop via 10.0.0.3 dev vxlan1100 onlink weight 1 \
+    nexthop via 10.0.0.4 dev vxlan1100 onlink weight 1
+
+ip neigh add 10.0.0.3 lladdr aa:bb:cc:dd:ee:03 dev vxlan1100 nud noarp extern_learn
+ip neigh add 10.0.0.4 lladdr aa:bb:cc:dd:ee:04 dev vxlan1100 nud noarp extern_learn
+
+bridge fdb add aa:bb:cc:dd:ee:03 dev vxlan1100 dst 10.0.0.3 self extern_learn
+bridge fdb add aa:bb:cc:dd:ee:04 dev vxlan1100 dst 10.0.0.4 self extern_learn
--- 22-symmetric/r4/vxlan.sh
+++ 22-symmetric/r4/vxlan.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#vrf blue
 ip link add vrf-blue type vrf table 1000
 ip link set vrf-blue up
 
@@ -17,3 +18,33 @@ ip neigh add 10.0.0.3 lladdr aa:bb:cc:dd:ee:03 dev vxlan1000 nud noarp extern_le
 
 bridge fdb add aa:bb:cc:dd:ee:02 dev vxlan1000 dst 10.0.0.2 self extern_learn
 bridge fdb add aa:bb:cc:dd:ee:03 dev vxlan1000 dst 10.0.0.3 self extern_learn
+
+#vrf red
+ip link add vrf-red type vrf table 1100
+ip link set vrf-red up
+
+#L2VNI
+ip link add br100 type bridge
+ip address add dev br100 192.168.3.2/24
+ip link set br100 master vrf-red addrgenmode none
+ip link add vxlan100 type vxlan id 100 local 10.0.0.4 dstport 4789
+ip link set addr aa:bb:cc:dd:03:02
+ip link set vxlan100 master br100 addrgenmode none
+ip link set eth3 master br100
+ip link set br100 up
+ip link set vxlan100 up
+
+bridge fdb add 00:00:00:00:00:00 dev vxlan100 dst 10.0.0.3 self extern_learn
+
+#L3VNI
+ip link add vxlan1100 type vxlan id 1100 local 10.0.0.4 dstport 4789 nolearning
+ip link set vxlan1100 addr aa:bb:cc:dd:ee:04
+ip link set vxlan1100 master vrf-red addrgenmode none
+ip link set vxlan1100 up
+
+#Type 5 evpn prefix route equivalent
+ip route add 192.168.2.0/24 via 10.0.0.2 dev vxlan1100 onlink vrf vrf-red
+
+ip neigh add 10.0.0.2 lladdr aa:bb:cc:dd:ee:02 dev vxlan1100 nud noarp extern_learn
+
+bridge fdb add aa:bb:cc:dd:ee:02 dev vxlan1100 dst 10.0.0.2 self extern_learn
